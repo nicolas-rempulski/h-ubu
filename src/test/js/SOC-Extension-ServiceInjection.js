386ce2579809dc9ce34d1a0158e4da0292fc90fc
Clement Escoffier <clement.escoffier@gmail.com>
2012-07-11 15:10:11 +0200

    Added a test about optional dependencies.

--- src/test/js/SOC-Extension-ServiceInjection.js
+++ src/test/js/SOC-Extension-ServiceInjection.js
@@ -19,7 +19,7 @@ describe("H-UBU Service Extension Tests - Service Injection and Lifecycle", func
         hub.reset();
     });
 
-    it("should support service chains", function() {
+    it("should support lifecycle", function() {
         var sumService = {
             sum : function(i1, i2) {}
         };
@@ -150,6 +150,153 @@ describe("H-UBU Service Extension Tests - Service Injection and Lifecycle", func
         hub.registerComponent(subtractServiceProvider);
         expect(main.doSomething()).toBe(4);
 
+    });
+
+    it("should support optional dependencies", function() {
+        var sumService = {
+            sum : function(i1, i2) {}
+        };
+
+        var subtractService = {
+            subtract : function(i1, i2) {}
+        };
+
+        var computationService = {
+            compute : function(i1, i2, i3) {}
+        };
+
+        var sumServiceProvider = {
+            hub: null,
+            configure : function(hub) {
+                this.hub = hub;
+                this.hub.provideService({
+                    component: this,
+                    contract: sumService
+                });
+            },
+            start : function () {},
+            stop : function () {},
+            getComponentName : function () { return "sum" },
+
+            sum : function (i1, i2) {
+                return i1 + i2;
+            }
+        };
+
+        var subtractServiceProvider = {
+            hub: null,
+            configure : function(hub) {
+                this.hub = hub;
+                this.hub.provideService({
+                    component: this,
+                    contract: subtractService
+                });
+            },
+            start : function () {},
+            stop : function () {},
+            getComponentName : function () { return "sub" },
+
+            subtract : function (i1, i2) {
+                return i1 - i2;
+            }
+        };
+
+        var computationServiceProvider = {
+            hub: null,
+            sum : null,
+            subtract : null,
+            configure : function(hub) {
+                this.hub = hub;
+                this.hub
+                    .provideService({
+                        component: this,
+                        contract: computationService
+                    })
+                    .requireService({
+                        component : this,
+                        contract : sumService,
+                        field : "sum"
+                    })
+                    .requireService({
+                        component : this,
+                        contract : subtractService,
+                        field : "subtract",
+                        optional : true
+                    });
+            },
+            start : function () {},
+            stop : function () {},
+            getComponentName : function () { return "computation-provider" },
+
+            compute : function (i1, i2, i3) {
+                if (this.subtract === null) {
+                    return this.sum.sum(i1, i2);
+                } else {
+                    return this.subtract.subtract(this.sum.sum(i1, i2), i3);
+                }
+            }
+        };
+
+        var main = {
+            hub : null,
+            computation : null,
+            configure : function(hub) {
+                this.hub = hub;
+                this.hub
+                    .requireService( {
+                        component : this,
+                        contract: computationService,
+                        bind: this.bind,
+                        unbind : this.unbind
+                    });
+            },
+            start : function() {},
+            stop : function() {},
+            getComponentName : function() { return "my-component"},
+
+            bind : function(svc) {
+                this.computation = svc;
+            },
+            unbind : function(svc) {
+                this.computation = null;
+            },
+
+            doSomething : function() {
+                if (this.computation !== null) {
+                    var r =  this.computation.compute(3, 2, 1);
+                    return r;
+                } else {
+                    return -1;
+                }
+            }
+        };
+
+        // Registration of all components
+        hub
+            .registerComponent(main)
+            .registerComponent(computationServiceProvider)
+            .registerComponent(sumServiceProvider)
+            .registerComponent(subtractServiceProvider)
+            .start();
+
+
+        expect(main.doSomething()).toBe(4);
+        // Unfortunately sub is leaving
+        hub.unregisterComponent(subtractServiceProvider);
+        // In this case it's an optional dependency returning 5
+        expect(main.doSomething()).toBe(5);
+
+        hub.unregisterComponent(sumServiceProvider);
+        // This time we're invalid
+        expect(main.doSomething()).toBe(-1);
+
+        hub.registerComponent(subtractServiceProvider);
+        // Still invalid
+        expect(main.doSomething()).toBe(-1);
+
+        hub.registerComponent(sumServiceProvider);
+        // Valid again
+        expect(main.doSomething()).toBe(4);
     });
 
 
